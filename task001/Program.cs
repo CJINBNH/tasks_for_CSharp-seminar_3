acd7d1aee03e3146f35cfe111bdead1d2fc7abf2
CJINBNH <alex_egorov_89@mail.ru>
2022-08-11 22:12:57 +0400

    Completed task003

--- task001/Program.cs
+++ task001/Program.cs
@@ -1,8 +1,6 @@
 ﻿//
 int[] point = new int[2];
 // int[] point = new int[2];
-// int x = 
-// int y = 
 // int point[0] = Convert.ToInt32(Console.ReadLine());
 // Console.WriteLine("Введите координату: x");
 // int x = Convert.ToInt32(Console.ReadLine());
---
+++ task003/Program.cs
@@ -0,0 +1,19 @@
+﻿// Задача 21: Напишите программу, которая принимает на вход координаты двух точек и находит 
+//расстояние между ними в 2D
+//A (3,6); B (2,1) -> 5,09
+//A (7,-5); B (1,-1) -> 7,21
+
+Console.WriteLine("Введите число x1: ");
+double X1 = Convert.ToDouble(Console.ReadLine());
+Console.WriteLine("Введите число у1: ");
+double У1 = Convert.ToDouble(Console.ReadLine());
+Console.WriteLine("Введите число x2: ");
+double X2 = Convert.ToDouble(Console.ReadLine());
+Console.WriteLine("Введите число у2: ");
+double У2 = Convert.ToDouble(Console.ReadLine());
+double length = Math.Sqrt((X2-X1)*(X2-X1)+(У2-У1)*(У2-У1));
+Console.WriteLine(length);
+
+// второе решение
+
+double length = Math.Sqrt(Math.Pow(X2-X1, 2) + Math.Pow(Y2-Y1, 2));
